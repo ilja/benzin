0e1cbe8cd70dab0b24f1ec0924d9272bf52c4e24
Ilja Krijger <ilja@quittheprogram.org>
2010-08-16 21:08:16 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tddbenzin::Application.config.secret_token = '8a9d32ff3f482f29f9e2b543b71fe5588c48b2fcf4a99e5fc9d4092b0817f4f6aee1dc5317164a404ea443164014e87740836292f189abbbdd57948180b9401f'
